8aa55d5b4ac3d117289ac8826ab199c64974b953
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-07 16:10:20 -0300

    #commit _ 9 - complemetando explicação sobre Interface.

--- src/Ferrari.java
+++ src/Ferrari.java
@@ -9,12 +9,14 @@ public class Ferrari implements Carro, ItemCaro { // Para que Ferrari tenha o co
 	@Override
 	public void virarEsquerda() {
 		System.out.println("Ferrari virou à esquerda");
-	}
+	} 													// A implementação está dando vida aos métodos da Interface.
 
 	@Override
 	public void virarDireita() {
 		System.out.println("Ferrari virou à direita");
-	}
+	} 													// Como a interface só define apenas o que é possível fazer.
+															// E é aqui na implementação da interface que você vai definir o "como".
+																// Ou seja, vai definir o "como" pode ser feito os métodos definidos na interface.
 
 	@Override
 	public void acelerar() {
