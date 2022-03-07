4bca7ae754a22e70b0261755c1a2611b079d61fd
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-07 15:49:05 -0300

    #commit _ 4

--- src/Automovel.java
+++ src/Automovel.java
@@ -1,7 +1,7 @@
 
 public interface Automovel { // Não se implementa métodos em interfaces. Existe uma excessao.
 
-	void virarEsquerda();
+	void virarEsquerda(); // É necessário entender que métodos dentro de uma interface, definem uma assinatura mas não os implementa.
 	void virarDireita();
 	void acelerar();
 	
