81c792aeb56fb277dfced3273e8a26d3c930183d
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-14 11:39:09 -0300

    # COMMIT - 5

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Autoboxing
-### Faz conversão de Classe Wrapper para Tipos Primitivos.
+# Classes Wrappers e Tipos primitivos: Trabalhando com o Autoboxing (Embrulhamento automático)
+### 1. Faz conversão de Classe Wrapper para Tipos Primitivos, e vice-versa, de forma automática.
+### 2. Autoboxing não muda as regras do Java.
+### 3. Realiza processo Imboxing: Embrulhamento automático.
+### 4. Funciona a partir do Java 9.
+### 5. Autoboxing elimina a necessidade de se trabalhar com valueOf().
--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -5,7 +5,7 @@ public class Aplicacao {
 		
 		Handler h = new Handler();
 
-		//Exemplo com java 4:
+		//Exemplo com java 4 com valueOf e sem autoboxing:
 		//Integer i = Integer.valueOf(10);
 		//h.setObj(i);
 		
@@ -13,29 +13,34 @@ public class Aplicacao {
 		//int i3 = i2.intValue();
 		//System.out.println(i3);
 
-		// Exemplo com Java 11:
-		//int i = 10;
-		//h.setObj(i);
+		// Exemplo com Java 11 (já funciona a partir do java 9):
+		//int i = 10; 						-> Aqui tenho um int
+		//h.setObj(i);						-> Dentro do parâmetro recebe um Integer.
+		//									-> Imboxing
 		
 		//int i2 = (int) h.getObj();
 		//System.out.println(i2);
-		
+
+		//Exemplo com java 4 com valueOf e sem autoboxing:
 		//Double d1 = Double.valueOf(5);
 		//Double d2 = Double.valueOf(4);
-		
-		//Double d1 = 5.0;
+
+		// Exemplo com Java 11:
+		//Double d1 = 5.0; 					-> Atribuindo um double (tipo primitivo) à um objeto Double (classe Wrapper). Só é possível pelo autoboxing.
 		//Double d2 = 4.0;
 		
-		//double d = somar(d1.doubleValue(), d2.doubleValue());
+		//double d = somar(d1.doubleValue(), d2.doubleValue());   -> Também funciona.
 		//Double d = somar(d1, d2);
 		//System.out.println(d);
-		
+
+		//Exemplo com java 4 com valueOf e sem autoboxing:
 		//Integer i = Integer.valueOf(5);
 		//int i2 = i.intValue();
 		//i2++;
 		//i = Integer.valueOf(i2);
 		//System.out.println(i);
-		
+
+		// Exemplo com Java 11:
 		Integer i = 5;
 		i++;
 		System.out.println(i);
